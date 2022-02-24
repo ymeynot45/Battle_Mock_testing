b3413a0adcc549a03f32116aa5d438beac26021d
William Karsten <yme_ynot45@hotmail.com>
2022-02-24 14:07:05 -0600

    rebuilt on click within a Cell. I need to figure out how to use the dispatch, I get missing token error.

--- src/Cell.js
+++ src/Cell.js
@@ -6,9 +6,15 @@ const Cell = function (cellId) {
   this.cellId = cellId
   this.cellRow = cellId.slice(1)
   this.cellCol = cellId.slice(0, 1)
+  const clickEffect = function (e) {
+    // const [, dispatch] = useReducer(reducer)
+    // dispatch({ type: this.cellId }
+    e.currentTarget.setAttribute('disabled', true)
+  }
+
   return (
-  <li key={cellId} id={cellId} className={'row' + this.cellRow + ' ' + 'boardCell'} > <button className='cellButton' variant="text" onClick={(e) => e.currentTarget.setAttribute('disabled', true)}> {this.cellCol + this.cellRow}</button></li>
-  ) // onClick={() => dispatch({ type: keyId })}
+  <li key={cellId} id={cellId} className={'row' + this.cellRow + ' ' + 'boardCell'} > <button className='cellButton' variant="text" onClick={(e) => clickEffect(e)}> {this.cellCol + this.cellRow}</button></li>
+  ) //
 }
 
 Cell.propTypes = {
